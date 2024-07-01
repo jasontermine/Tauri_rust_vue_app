fc449d181c846a1a88fef6ec9b2e50aa69522791
jasontermine <jason.termine@wiss-edu.ch>
2024-07-01 16:31:28 +0200

    UPDATED: added mdbootstrap

--- src/components/Nav/Nav.vue
+++ src/components/Nav/Nav.vue
@@ -1,9 +1,31 @@
 <template>
-  <nav class="navbar navbar-dark bg-dark">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
-    </div>
-  </nav>
+  <MDBNavbar light bg="light" expand="lg" container>
+    <MDBNavbarBrand href="#">Navbar</MDBNavbarBrand>
+    <MDBNavbarToggler
+      target="#navbarNav"
+      @click="collapse2 = !collapse2"
+    ></MDBNavbarToggler>
+    <MDBCollapse id="navbarNav" v-model="collapse2">
+      <MDBNavbarNav>
+        <MDBNavbarItem to="#" active> Home </MDBNavbarItem>
+        <MDBNavbarItem to="#"> Features </MDBNavbarItem>
+        <MDBNavbarItem to="#"> Pricing </MDBNavbarItem>
+        <MDBNavbarItem to="#" disabled> Disabled </MDBNavbarItem>
+      </MDBNavbarNav>
+    </MDBCollapse>
+  </MDBNavbar>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import {
+    MDBNavbar,
+    MDBNavbarToggler,
+    MDBNavbarBrand,
+    MDBNavbarNav,
+    MDBNavbarItem,
+    MDBCollapse,
+  } from 'mdb-vue-ui-kit';
+  import { ref } from 'vue';
+
+  const collapse2 = ref(false);
+</script>
