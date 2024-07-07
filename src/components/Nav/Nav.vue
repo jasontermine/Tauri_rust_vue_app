2627b7de11d01dbe633cc19dfa425cb7ea072ce0
jasontermine <jason.termine@wiss-edu.ch>
2024-07-07 09:04:46 +0200

    UPDATED: changed nav from MDBootstrap to Vuetify example nav

--- src/components/Nav/Nav.vue
+++ src/components/Nav/Nav.vue
@@ -1,31 +1,37 @@
 <template>
-  <MDBNavbar light bg="light" expand="lg" container>
-    <MDBNavbarBrand href="#">Navbar</MDBNavbarBrand>
-    <MDBNavbarToggler
-      target="#navbarNav"
-      @click="collapse2 = !collapse2"
-    ></MDBNavbarToggler>
-    <MDBCollapse id="navbarNav" v-model="collapse2">
-      <MDBNavbarNav>
-        <MDBNavbarItem to="#" active> Home </MDBNavbarItem>
-        <MDBNavbarItem to="#"> Features </MDBNavbarItem>
-        <MDBNavbarItem to="#"> Pricing </MDBNavbarItem>
-        <MDBNavbarItem to="#" disabled> Disabled </MDBNavbarItem>
-      </MDBNavbarNav>
-    </MDBCollapse>
-  </MDBNavbar>
+  <VLayout>
+    <VNavigationDrawer expand-on-hover rail>
+      <VList>
+        <VListItem
+          prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+          subtitle="sandra_a88@gmailcom"
+          title="Sandra Adams"
+        ></VListItem>
+      </VList>
+
+      <VDivider />
+
+      <VList density="compact" nav>
+        <VListItem
+          prepend-icon="mdi-folder"
+          title="My Files"
+          value="myfiles"
+        ></VListItem>
+        <VListItem
+          prepend-icon="mdi-account-multiple"
+          title="Shared with me"
+          value="shared"
+        ></VListItem>
+        <VListItem
+          prepend-icon="mdi-star"
+          title="Starred"
+          value="starred"
+        ></VListItem>
+      </VList>
+    </VNavigationDrawer>
+
+    <v-main style="height: 250px"></v-main>
+  </VLayout>
 </template>
 
-<script setup lang="ts">
-  import {
-    MDBNavbar,
-    MDBNavbarToggler,
-    MDBNavbarBrand,
-    MDBNavbarNav,
-    MDBNavbarItem,
-    MDBCollapse,
-  } from 'mdb-vue-ui-kit';
-  import { ref } from 'vue';
-
-  const collapse2 = ref(false);
-</script>
+<script setup lang="ts"></script>
