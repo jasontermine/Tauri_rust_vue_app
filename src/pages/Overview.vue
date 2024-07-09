f5a26b9809ce8b0858c870a8775207a2de56cd49
jasontermine <jason.termine@wiss-edu.ch>
2024-07-09 18:37:30 +0200

    UPDATED: moved FetchRepos to Overview.vue

---
+++ src/pages/Overview.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <FetchRepos />
+  </div>
+</template>
+
+<script setup lang="ts">
+import FetchRepos from '@/components/FetchRepos/FetchRepos.vue';
+
+</script>
+
+<style scoped>
+.container {
+  margin-top: 5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
