1fbb887b883b7e566a7ebc4a1180704e74fecf84
jasontermine <jason.termine@wiss-edu.ch>
2024-07-09 19:45:07 +0200

    UPDATED: added pinia

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import App from '@/App.vue'
 import router from '@/router';
+import { createPinia } from 'pinia';
 
+const pinia = createPinia()
 const app = createApp(App);
 
 const vuetify = createVuetify({
@@ -13,6 +15,7 @@ const vuetify = createVuetify({
   directives,
 })
 
+app.use(pinia)
 app.use(router);
 app.use(vuetify);
 app.mount('#app');
