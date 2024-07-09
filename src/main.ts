ba30b80cc571e21ab99cee817d8302f124872348
jasontermine <jason.termine@wiss-edu.ch>
2024-07-09 18:37:08 +0200

    UPDATED: refactored

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,16 @@ import { createApp } from "vue";
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-import App from './App.vue'
+import App from '@/App.vue'
+import router from '@/router';
+
+const app = createApp(App);
 
 const vuetify = createVuetify({
   components,
   directives,
 })
 
-createApp(App).use(vuetify).mount('#app')
+app.use(router);
+app.use(vuetify);
+app.mount('#app');
